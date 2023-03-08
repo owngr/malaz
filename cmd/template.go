bc9635effb1b86198f28165345ef0322ad13a100
Olivier Wenger <olivier.wenger@liip.ch>
2023-03-08 21:54:38 +0100

    base templating works

--- cmd/template.go
+++ cmd/template.go
@@ -5,7 +5,12 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
+	"text/template"
+
+	"github.com/go-yaml/yaml"
 	"github.com/spf13/cobra"
 )
 
@@ -21,8 +26,37 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("test")
+		values := loadValues()
+		template := loadTemplate()
+		template.Execute(os.Stdout, values)
 	},
+}
+
+func loadValues() map[string]interface{} {
+	data, err := ioutil.ReadFile("values.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a map to hold the YAML data
+	var config map[string]interface{}
+
+	// Unmarshal the YAML data into the map
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(config)
+	return config
+}
+
+func loadTemplate() *template.Template {
+	t, err := template.ParseFiles("templates/hello.go.tmpl")
+
+	if err != nil {
+		panic(err)
+	}
+	return t
 }
 
 func init() {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module malaz
 go 1.19
 
 require (
+	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
